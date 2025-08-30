ac58a54269809f8f85bde4511dc791fe19a74cc0
Javad Mohammadi <javadsemail@gmail.com>
2025-08-30 16:20:42 +0330

    ♻️ Refactor ErrorTypes

--- internal/constants/error_messeges.go
+++ internal/constants/error_messeges.go
@@ -1,12 +1,12 @@
 package constants
 
-const (
-	ErrIDParamNotProvided = "id not provided"
-	ErrInvalidIDParam     = "invalid id"
-)
+type ErrorType uint
 
 const (
-	ErrAuthUserNotFound        = "user not found"
-	ErrAuthUnauthorized        = "unauthorized"
-	ErrAuthGenerateTokenFailed = "failed to generate token"
+	ErrIDParamNotProvided ErrorType = iota
+	ErrInvalidIDParam
+
+	ErrAuthUserNotFound
+	ErrAuthUnauthorized
+	ErrAuthGenerateTokenFailed
 )
